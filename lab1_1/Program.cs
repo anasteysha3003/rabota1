3e4ab94438ccb9163c7484133de23b184f940ce0
yfcnz <yfcnz@511-22>
2026-09-12 14:55:04 +0200

    Добавьте файлы проекта.

---
+++ lab1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.Write("Введите х:");
+//double x = double.Parse(Console.ReadLine());
+//double K = (Math.Sqrt(Math.Pow(3 + x, 6) - Math.Log(x))) / (Math.Exp(0) + Math.Asin(6 * x * x));
+//Console.WriteLine($"K={K:f2}");
+
+Console.Write("Введите х:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите c:");
+double c = double.Parse(Console.ReadLine());
+double P = (Math.Pow(a, 5) + Math.Acos(a + Math.Pow(x, 3))) - (Math.Pow(Math.Sin(y-c),4)) / (Math.Pow(Math.Sin(x+y),3) + Math.Abs(x-y)) ;
+Console.WriteLine($"P = {P:F2}");
---
+++ rabota1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Console.Write("Введите ваше имя:");
+//string name = Console.ReadLine();
+//Console.WriteLine("Меня зовут" + name);
+//Console.WriteLine("Меня зовут {0} ",name);
+//Console.WriteLine($"Меня зовут {name}");
+
+//Console.Write("Введите х:");
+//int x = int.Parse(Console.ReadLine());
+//Console.Write("Введите у:");
+//int y = int.Parse(Console.ReadLine());
+//int s = y + x;
+//Console.WriteLine($"{x}+{y}={s}");
+
+//Console.Write("Введите угол в градусах:");
+//double angle = double.Parse(Console.ReadLine());
+//double y = Math.Sin(angle*Math.PI/180);
+//Console.WriteLine($"y={y:F2}");
+
+//Console.Write("Введите степень числа:");
+//double number = double.Parse(Console.ReadLine());
+//double y = Math.Exp(7*number);
+//Console.WriteLine($"y={y:f2}");
