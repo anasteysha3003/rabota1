20e33938f40a2853e6df9430ac0441bf1370d39d
yfcnz <yfcnz@511-22>
2026-09-19 14:38:39 +0200

    3_1

--- lab1_1/Program.cs
+++ lab1_1/Program.cs
@@ -16,18 +16,31 @@
 //Console.WriteLine($"P = {P:F2}");
 
 //Лабораторная работа 1.2
+//try
+//{
+//    Console.Write("Введите число сторон n:");
+//    double n = double.Parse(Console.ReadLine());
+//    Console.Write("Введите длинну стороны а:");
+//    double a = double.Parse(Console.ReadLine());
+//    double R = a / (2 * Math.Sin(Math.PI / n));
+//    double r = a / (Math.Tan(Math.PI / n));
+//    Console.WriteLine($"R = {R:f2}");
+//    Console.WriteLine($"r = {r:f2}");
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//Лабораторная работа 1.3
 try
 {
-    Console.Write("Введите число сторон n:");
-    double n = double.Parse(Console.ReadLine());
-    Console.Write("Введите длинну стороны а:");
-    double a = double.Parse(Console.ReadLine());
-    double R = a / (2 * Math.Sin(Math.PI / n));
-    double r = a / (Math.Tan(Math.PI / n));
-    Console.WriteLine($"R = {R:f2}");
-    Console.WriteLine($"r = {r:f2}");
+    Console.Write("Введите число n, при 1<=n<=999: ");
+    int n = int.Parse(Console.ReadLine());
+    int x = (n % 10) * 100 + (n / 10);
+    Console.WriteLine($"Начальное число х: {x}");
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
--- rabota1/Program.cs
+++ rabota1/Program.cs
@@ -20,3 +20,94 @@
 //double number = double.Parse(Console.ReadLine());
 //double y = Math.Exp(7*number);
 //Console.WriteLine($"y={y:f2}");
+
+//Console.WriteLine($"{Math.PI:f2}");
+
+//Console.WriteLine($"{Math.E:f1}");
+
+//Console.Write("Введите число а:");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Вы ввели число {a}");
+
+//Console.WriteLine("1 13 49");
+//Console.WriteLine("7  15  100");
+
+//try
+//{
+//    Console.Write("Введите а:");
+//    double a = double.Parse(Console.ReadLine());
+//    Console.Write("Введите в:");
+//    double b = double.Parse(Console.ReadLine());
+//    Console.Write("Введите c:");
+//    double c = double.Parse(Console.ReadLine());
+//    double d = ((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+//    Console.WriteLine($"{d:f2}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//1
+//try
+//{
+//    Console.Write("Введите расстояние в см:");
+//    int sm = int.Parse(Console.ReadLine());
+//    int m = sm / 100;
+//    Console.WriteLine($"Полных метров:{m}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//2
+//Console.Write("Введите массу в кг:");
+//int kg = int.Parse(Console.ReadLine());
+//int ct = kg / 100;
+//Console.WriteLine($"Полных центнеров:{ct}");
+//3
+//Console.Write("Введите массу в кг:");
+//int kg = int.Parse(Console.ReadLine());
+//int t = kg / 1000;
+//Console.WriteLine($"Полных тонн:{t}");
+//4
+//Console.Write("Введите расстояние в метрах:");
+//int m = int.Parse(Console.ReadLine());
+//int km = m / 1000;
+//Console.WriteLine($"Полных километров:{km}");
+//5
+//Console.Write("С некоторого момента прошло 234 дня, значит ");
+//int n = 234 / 7;
+//Console.WriteLine($"Полных недель:{n}");
+//6
+//Console.Write("Введите n:");
+//int n = int.Parse(Console.ReadLine());
+//int hour = n / 3600;
+//int minute = (n % 3600) / 60;
+//int second =  n % 60;
+//Console.WriteLine($"а)Полных часов с начала суток: {hour}");
+//Console.WriteLine($"б)Полных минут с начала очередного часа: {minute}");
+//Console.WriteLine($"в)Полных секунд с начала очередной минуты: {second}");
+7
+//Console.Write("прямоугльник размерами 543 на 130 мм можно разделить на ");
+//int nk = (543 * 130) / (130 * 130);
+//Console.WriteLine($"{nk} квадрата.");
+//8
+//try 
+//{ 
+//Console.Write("Введите четырехзначное число: ");
+//int n = int.Parse(Console.ReadLine());
+//int e = n % 10;
+//int d = n % 100 / 10;
+//int s = n % 1000 / 100;
+//int t = n / 1000;
+//int sum = e + d + s + t;
+//Console.WriteLine ($"колличество едениц: {e}");
+//Console.WriteLine($"колличество десятков: {d}");
+//Console.WriteLine($"колличество сотен: {s}");
+//Console.WriteLine($"колличество тысяч: {t}");
+//Console.WriteLine($"сумма цифр числа: {sum}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
